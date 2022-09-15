371d0cd2e89e44500d056134f138c609d5bc389b
Taro Saika <tarosaika@Taros-MacBook-Pro.local>
2022-09-16 01:04:57 +0900

    Add lists of projects in /projects

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -27,6 +27,33 @@ export default function Projects() {
             >
               <Link href="/" >Go home</Link>
             </h1>
+            <li>
+              <ul>
+                <Link href="/projects/1" >Paper animation</Link>
+              </ul>
+              <ul>
+                <Link href="/projects/2" >Map animation</Link>
+              </ul>
+              <ul>
+                <Link href="/projects/3" >Magnifier</Link>
+              </ul>
+              <ul>
+                <Link href="/projects/4" >Text animation</Link>
+              </ul>
+              <ul>
+                <Link href="/projects/5" >Keyboard animation</Link>
+              </ul>
+              <ul>
+                <Link href="/projects/6" >Meteor</Link>
+              </ul>
+              <ul>
+                <Link href="/projects/7" >Urban city</Link>
+              </ul>
+              <ul>
+                <Link href="/projects/8" >Cheering up</Link>
+              </ul>
+             
+            </li>
           </div>
         </div>
 
