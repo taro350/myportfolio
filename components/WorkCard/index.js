ea07d6b66cefd9b2cf7a0550d3e5f987486e70b6
Taro Saika <tarosaika@Taros-MacBook-Pro.local>
2022-09-22 16:12:32 +0900

    Change the layout of projects in '/'

--- components/WorkCard/index.js
+++ components/WorkCard/index.js
@@ -1,20 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-const WorkCard = ({ img, name, description, onClick }) => {
+export default function WorkCard ({ img, name, description, link }) {
   return (
-    <div
-      className="overflow-hidden rounded-lg p-2 laptop:p-4 first:ml-0 link"
-      onClick={onClick}
+    <Link
+      href={link}
+      passHref
+      className="ovserflow-hidden rounded-lg p-2 laptop:p-4 first:ml-0 link"
     >
+      <BaseCard img={img} name={name} description={description} link={link}/>
+    </Link>
+  );
+};
+
+const BaseCard = React.forwardRef( ({img, name, description, link}, fref) => 
+  (
+    <a href={link} ref={fref}>
       <div
-        className="relative rounded-lg overflow-hidden transition-all ease-out duration-300 h-48 mob:h-auto"
-        style={{ height: "600px" }}
+        className="relative h-48 mob:h-auto
+        rounded-lg overflow-hidden 
+        transition-all ease-out duration-300 "
       >
-        <img
-          alt={name}
-          className="h-full w-full object-cover hover:scale-110 transition-all ease-out duration-300"
+        <Image
           src={img}
-        ></img>
+          alt={name}
+          width="100%" height="24rem" layout="responsive"
+          objectFit="cover"
+          className="
+          rounded-lg shadow-lg
+          [hover:scale-110 transition-all ease-out duration-300]
+          "
+        />
       </div>
       <h1 className="mt-5 text-3xl font-medium">
         {name ? name : "Project Name"}
@@ -22,8 +39,8 @@ const WorkCard = ({ img, name, description, onClick }) => {
       <h2 className="text-xl opacity-50">
         {description ? description : "Description"}
       </h2>
-    </div>
-  );
-};
+      </a>
+  )
+)
 
-export default WorkCard;
+BaseCard.displayName = "BaseCard"; 
--- pages/index.js
+++ pages/index.js
@@ -118,7 +118,7 @@ export default function Home() {
                 img={project.imageSrc}
                 name={project.title}
                 description={project.description}
-                onClick={() => window.open(project.url)}
+                link={project.url}
               />
             ))}
           </div>
